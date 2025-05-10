ca37c269a27161be19601702ae0896f21373e98e
Naufall0000 <swastikanaufal@gmail.com>
2025-05-10 22:49:00 +0800

    Menambahkan fungsi slideshow gambar otomatis di bagian hero

---
+++ js/homepage.js
@@ -0,0 +1,20 @@
+// funcion slide gambar di hero
+const heroSection = document.querySelector('.hero');
+const images = [
+    'assets/kecaksunset.jpeg',
+    'assets/sawahubud.jpeg',
+    'assets/klingkingbeach.jpeg',
+    'assets/tamanujung2.jpeg',
+    'assets/danaubatur2.jpeg',
+    'assets/pantailovina2.jpeg'
+  ];
+
+let current = 0;
+
+function changeBackground() {
+  heroSection.style.backgroundImage = `url(${images[current]})`;
+  current = (current + 1) % images.length;
+}
+
+changeBackground();
+setInterval(changeBackground, 3000);
