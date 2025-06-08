861e1e6f270dd9334f9f7ca14227d36ede27e552
pratama372 <pratamayasaputra@gmail.com>
2025-06-08 09:08:01 +0800

    menambahkan markers pada file denpasar.js

--- js/denpasar.js
+++ js/denpasar.js
@@ -18,3 +18,39 @@ const customIcon = L.icon({
     iconAnchor: [16, 32],
     popupAnchor: [0, -32]
 });
+
+// Add markers
+const markers = [
+  {
+    coords: [-8.6575, 115.2185],  // Museum Bali
+    title: "Museum Bali",
+    desc: "Museum sejarah dan budaya Bali dengan koleksi arkeologi, seni, dan etnografi. Terletak di pusat kota Denpasar."
+  },
+  {
+    coords: [-8.6830, 115.2670],  // Pantai Sanur
+    title: "Pantai Sanur",
+    desc: "Pantai tenang yang cocok untuk melihat matahari terbit dan aktivitas keluarga. Favorit wisatawan lokal dan mancanegara."
+  },
+  {
+    coords: [-8.6564, 115.2122],  // Pasar Badung
+    title: "Pasar Badung",
+    desc: "Pasar tradisional terbesar di Bali, tempat membeli oleh-oleh, kain, dan rempah khas Bali."
+  },
+  {
+   coords: [-8.6829, 115.2359],  // I AM Bali 3D Museum & Upside Down Zone
+    title: "I AM Bali 3D Museum & Upside Down Zone",
+    desc: "Museum seni interaktif dengan lebih dari 100 lukisan 3D dan zona rumah terbalik. Terletak di lantai dasar Monumen Bajra Sandhi, Renon, Denpasar."
+  }
+];
+
+
+
+markers.forEach(marker => {
+    L.marker(marker.coords, { icon: customIcon }).addTo(map)
+        .bindPopup(`<b>${marker.title}</b><br>${marker.desc}`)
+        .openPopup();
+});
+
+// Fit map to show all markers
+const group = new L.featureGroup(markers.map(m => L.marker(m.coords)));
+map.fitBounds(group.getBounds().pad(0.2));
