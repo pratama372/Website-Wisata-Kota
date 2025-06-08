3ddef82158b9bc92cb6aaba2331318a17e20a62f
pratama372 <pratamayasaputra@gmail.com>
2025-06-08 09:11:07 +0800

    menambahkan markers pada file gianyar.js

--- js/gianyar.js
+++ js/gianyar.js
@@ -18,3 +18,37 @@ const customIcon = L.icon({
     iconAnchor: [16, 32],
     popupAnchor: [0, -32]
 });
+
+// Add markers
+const markers = [
+    {
+        coords: [ -8.4156,  115.3187],  // Coordinates for Pura Tirta Empul
+        title: "Pura Tirta Empul",
+        desc: "Pura pembersihan diri yang terkenal dengan mata air sucinya, tempat ritual melukat (mandi suci). Lokasi di Tampaksiring."
+    },
+    {
+        coords: [-8.4269, 115.2763],  // Coordinates for Tegallalang Rice Terrace
+        title: "Sawah Terasering Tegallalang",
+        desc: "Pemandangan sawah berundak yang spektakuler, contoh sempurna sistem subak warisan UNESCO."
+    },
+    {
+        coords: [-8.5238, 115.2879],  // Coordinates for Goa Gajah
+        title: "Goa Gajah",
+        desc: "Situs arkeologi abad ke-11 dengan gua batu yang diukir dan kolam pemandian kuno. Terletak di Desa Bedulu."
+    },
+    {
+        coords: [-8.5069, 115.2620],  // Coordinates for Ubud Art Market
+        title: "Pasar Seni Ubud",
+        desc: "Pasar tradisional yang menjual berbagai kerajinan tangan, seni, dan oleh-oleh khas Bali. Buka setiap hari di jantung Ubud."
+    }
+];
+
+markers.forEach(marker => {
+    L.marker(marker.coords, { icon: customIcon }).addTo(map)
+        .bindPopup(`<b>${marker.title}</b><br>${marker.desc}`)
+        .openPopup();
+});
+
+// Fit map to show all markers
+const group = new L.featureGroup(markers.map(m => L.marker(m.coords)));
+map.fitBounds(group.getBounds().pad(0.2));
