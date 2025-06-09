7126a0428bd2ca607c318002dc81e5ad8507b542
Shoma097 <shomawhimbardhi@gmail.com>
2025-06-09 08:49:11 +0800

    menambahkan custom icon pada bangli.js

--- js/bangli.js
+++ js/bangli.js
@@ -52,3 +52,12 @@ const map = L.map('mapid', {
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// Add custom icon
+const customIcon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+});
+
