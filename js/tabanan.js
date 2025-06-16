014803e91062de6a09a15c6dd56ee48574bef2b6
bobythebaby <albertdarmawan7@gmail.com>
2025-06-16 21:36:11 +0800

    menambahkan fitur peta pada bagian js tabanan

---
+++ js/tabanan.js
@@ -0,0 +1,7 @@
+// Initialize map
+const map = L.map("mapid", {
+  center: [-8.35, 115.1], // Centered around Bali's central highlands
+  zoom: 10,
+  scrollWheelZoom: true,
+  zoomControl: false,
+});
